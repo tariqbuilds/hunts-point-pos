6ae75b283570b33825b546cc8b6b2f3308d4cbcd
Afaq Tariq <afaq05@gmail.com>
2015-03-04 00:10:54 -0500

    added basic pages for inventory section

--- public/js/pos.js
+++ public/js/pos.js
@@ -14,15 +14,14 @@ pos.config(['$routeProvider',
             templateUrl: 'templates/inventory.html',
             controller: 'inventoryController',
           }).
-          when('/inventory/item/new', {
-            templateUrl: 'templates/inventory/new-item.html',
-            controller: 'newItemController',
+          when('/inventory/create-product', {
+            templateUrl: 'templates/inventory/create-product.html',
+            controller: 'newProductController',
           }).
-          when('/inventory/item/:itemId', {
-            templateUrl: 'templates/inventory.html',
-            controller: 'editItemController',
+          when('/inventory/product/:productId', {
+            templateUrl: 'templates/inventory/edit-product.html',
+            controller: 'editProductController',
           }).
-          
           when('/pos', {
             templateUrl: 'templates/pos.html',
           }).
@@ -32,37 +31,95 @@ pos.config(['$routeProvider',
           
     }]);
 
-////////////////// Controllers //////////////////
+///////////////////////////////////////////////////
+////////////////// Directives ////////////////// //
+////////////////////////////////////////////////////
+
+pos.directive('productForm',function ($location) {
+  return {
+    restrict: 'E',
+    scope: {
+      product: '=',
+      onSave: '&'
+    },
+    templateUrl: 'templates/directives/product-form.html',
+    link: function (scope, el) {
+
+      // highlight barcode field
+      el.find('form').eq(0).find('input').eq(0).select();
+
+      scope.save = function () {
+        console.log('saving form');
+        scope.onSave({ product: scope.product });
+      };
+
+    }
+  };
+
+});
+
+/////////////////////////////////////////////////////
+////////////////// Controllers ////////////////// //
+////////////////////////////////////////////////////
 
 pos.controller('body', function ($scope) {
 });
 
 // Inventory Section
 
-pos.controller('inventoryController', function ($scope) {
+pos.controller('inventoryController', function ($scope, $location) {
 
+  // get and set inventory
   $scope.inventory = [
     {
       id: 48,
       name: "Product 1",
       price: 13.21,
-      quanity_on_hand: 259,
+      quantity_on_hand: 259,
     },
     {
       id: 21,
       name: "Product 2",
       price: 25.34,
-      quanity_on_hand: 2,
+      quantity_on_hand: 2,
     },
     {
       id: 32,
       name: "Product 3",
       price: 32.00,
-      quanity_on_hand: 15,
+      quantity_on_hand: 15,
     },
   ];
+
+  // go to edit page
+  $scope.editProduct = function (productId) {
+    $location.path('/inventory/product/' + productId);
+  };
+
 });
 
-pos.controller('newItemController', function ($scope) {
+pos.controller('newProductController', function ($scope, $location) {
+  
+  $scope.createProduct = function (product) {
+    // call api to create product
+    console.log(product);
+
+    // go back to inventory
+    $location.path('/inventory');
+  };
+
+});
+
+pos.controller('editProductController', function ($scope, $location) {
+  
+  $scope.saveProduct = function (product) {
+    // call api to save product
+    console.log(product);
+
+    // go back to inventory
+    $location.path('/inventory');
+  };
+
+  $scope.product = {barcode: "kjnfkjnkgndk", name: "lsglnl", price: 14.1, quantity_on_hand: 8};
 
 });
