79100270989ff1ce2d3593c3e2774fd2d930a811
Afaq Tariq <afaq05@gmail.com>
2015-03-04 18:18:06 -0500

    api add product to inventory working. get products from inventory as well.

--- server/api/inventory.js
+++ server/api/inventory.js
@@ -1,31 +1,42 @@
-var app 	= require('express')();
-var server 	= require('http').Server(app);
-var bodyParser = require('body-parser');
-var Datastore = require('nedb');
+var app 	= require('express')()
+var server 	= require('http').Server(app)
+var bodyParser = require('body-parser')
+var Datastore = require('nedb')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-module.exports = app;
+module.exports = app
 
 // Database stuff
-var inventoryDB = new Datastore({ filename: './server/databases/inventory.db', autoload: true });
+var inventoryDB = new Datastore({ 
+	filename: './server/databases/inventory.db', 
+	autoload: true 
+})
 
 // GET all inventory items
 app.get('/', function (req, res) {
-	res.send('Inventory API');
-});
+	res.send('Inventory API')
+})
 
 // GET all inventory items
 app.get('/products', function (req, res) {
+
 	inventoryDB.find({}, function (err, docs) {
-		res.send(docs);
-	});
-});
+		res.send(docs)
+	})
+})
 
 // Create inventory product
 app.post('/product', function (req, res) {
 
-	console.log(req.query);
-	console.log(req.body);
-	res.sendStatus(200);
-});
+	var newProduct = req.body
+	
+	inventoryDB.insert(newProduct, function (err, product) {
+		if (err) {
+			res.write(err)
+			res.sendStatus(500)
+		}
+
+		res.sendStatus(200);
+	})
+})
